e8bb394ad7b1105fa964f092b2733f1254312f78
ahmadullahtonmoy <ahmadullahtonmoy@gmail.com>
2022-01-03 11:24:18 +0600

    screens added

---
+++ lib/screens/feeds_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class FeedsScreen extends StatelessWidget {
+  static const routeName = '/Feeds-screen';
+
+  const FeedsScreen({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
---
+++ lib/screens/user_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class UserScreen extends StatelessWidget {
+  static const routeName = '/User-screen';
+
+  const UserScreen({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
