c14813fd3fdf5042763ed7419bd473e0cd4918c3
ahmadullahtonmoy <ahmadullahtonmoy@gmail.com>
2022-01-17 11:52:03 +0600

    added botto nav screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_shop_cpt21/screens/bottom_nav_screen.dart';
 import 'package:flutter_shop_cpt21/screens/cart_screen.dart';
 import 'package:flutter_shop_cpt21/screens/feeds_screen.dart';
 import 'package:flutter_shop_cpt21/screens/home_screen.dart';
@@ -17,11 +18,13 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       routes: {
+        BottomNavSceer.routeName: (ctx) => const BottomNavSceer(),
         HomeScreen.routeName: (ctx) => const HomeScreen(),
         FeedsScreen.routeName: (ctx) => const FeedsScreen(),
         SearchScreen.routeName: (ctx) => const SearchScreen(),
         CartScreen.routeName: (ctx) => const CartScreen(),
         UserScreen.routeName: (ctx) => const UserScreen(),
+
       },
     );
   }
---
+++ lib/screens/bottom_nav_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class BottomNavSceer extends StatefulWidget {
+  static const routeName = '/Bottom-nav-screen';
+  const BottomNavSceer({ Key? key }) : super(key: key);
+
+  @override
+  _BottomNavSceerState createState() => _BottomNavSceerState();
+}
+
+class _BottomNavSceerState extends State<BottomNavSceer> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
